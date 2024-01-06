ed699af7ef52193cec45dfc74fbbefac81b1854c
Ariel Leyva <aleyva1509@gmail.com>
2024-01-06 17:48:05 -0600

    add: hello world example

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth  = 300
+	screenHeight = 300
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw() error {
+	return nil
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return screenWidth, screenHeight
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Hello, World!")
 }
