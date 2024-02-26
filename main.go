1fcc9f8710717ef27adae357172803c5f39a1f85
Ariel Leyva <aleyva1509@gmail.com>
2024-02-26 06:36:35 -0600

    add: hello world now works!

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
@@ -18,16 +19,19 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw() error {
-	return nil
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return screenWidth, screenHeight
+	return 300, 300
 }
 
 func main() {
 	fmt.Println("Hello, World!")
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		panic(err)
+	}
 }
